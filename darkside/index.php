772cd1c35a3dc5c7e66dc48e11b429e2b4378296
Jan Marsch <jama@keks.com>
2013-03-14 01:36:11 +0100

    viewport adjusted

--- darkside/index.php
+++ darkside/index.php
@@ -2,6 +2,7 @@
 <html>
 <head>
     <title>OSM Buildings - Building shadows</title>
+    <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=0">
     <meta http-equiv="content-type" content="text/html; charset=utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <link rel="stylesheet" href="../assets/fullscreen.css">
