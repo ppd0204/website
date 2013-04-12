82ad829cf08af0330f2fb70868ce99f343a55f1b
Jan Marsch <jama@keks.com>
2013-04-12 11:21:46 +0200

    adding acid example

---
+++ acid/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>OSM Buildings - Acid</title>
+    <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=0">
+    <meta http-equiv="content-type" content="text/html; charset=utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<link rel="icon" type="image/png" href="../favicon.png">
+	<link rel="stylesheet" href="../assets/fullscreen.css">
+    <link rel="stylesheet" href="../js/leaflet-0.5.1/leaflet.css">
+    <script src="../js/Fly.js"></script>
+    <script src="../js/leaflet-0.5.1/leaflet.js"></script>
+    <script src="L.BuildingsLayer-acid.js"></script>
+    <script src="../js/scripts.js"></script>
+</head>
+
+<body>
+    <div id="map"></div>
+    <a href="http://osmbuildings.org/"><img src="../logo.png" class="logo"></a>
+    <script>
+	tilesURL = 'black.png';
+	Fly.on('ready', initMap);
+	</script>
+	<script src="../js/piwik.js"></script>
+</body>
+</html>
