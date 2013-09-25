a9d2fdc99fd4d920f6199e5b6b7dc3ab5e705166
Jan Marsch <jama@keks.com>
2013-09-26 01:02:43 +0200

    leaflet draw updated

--- examples/Drawing.php
+++ examples/Drawing.php
@@ -8,8 +8,8 @@ require_once("$root/_base.php");
 <link rel="stylesheet" href="<?=ROOT?>/js/highlight-7.3/styles/github-code.css">
 <script src="<?=ROOT?>/js/highlight-7.3/highlight.pack.js"></script>
 
-<link rel="stylesheet" href="<?=ROOT?>/js/leaflet.draw/leaflet.draw.css">
-<script src="<?=ROOT?>/js/leaflet.draw/leaflet.draw.js"></script>
+<link rel="stylesheet" href="<?=ROOT?>/js/leaflet.draw-0.2.0/leaflet.draw.css">
+<script src="<?=ROOT?>/js/leaflet.draw-0.2.0/leaflet.draw.js"></script>
 <script>mapOptions.loadData = false</script>
 
 <h1>Drawing</h1>
@@ -18,12 +18,6 @@ require_once("$root/_base.php");
 Once you're done, extrude it by using the height slider</p>
 
 <p>
-<div class="colorset">Color:
-<button class="color" style="background:#FADC64;" onclick="setColor(this)"></button>
-<button class="color" style="background:#6464FA;" onclick="setColor(this)"></button>
-<button class="color" style="background:#FA6464;" onclick="setColor(this)"></button>
-</div>
-
 Height:
 <input type="text" value="300" maxlength="3" size="3" onkeyup="setHeight(this)">
 </p>
@@ -36,20 +30,6 @@ var osmb = new OSMBuildings(map);
 ")?></code>
 
 <script>
-// creating data object here for adding features
-var geoJson = {
-  type: 'FeatureCollection',
-  features: []
-};
-
-function setColor(el) {
-  var polygonHandler = drawControl.handlers.polygon;
-  color = el.style.backgroundColor;
-  // hack: LeafletDraw doesn't allow setting a color with immediate effect
-  polygonHandler.options.shapeOptions.color = color;
-  polygonHandler.enable.call(polygonHandler);
-}
-
 function setHeight(el) {
   height = parseInt(el.value) || 50;
 }
@@ -59,40 +39,42 @@ var drawControl,
   height = 300;
 
 document.addEventListener('DOMContentLoaded', function() {
+  var drawnItems = new L.FeatureGroup();
+  map.addLayer(drawnItems);
+
   drawControl = new L.Control.Draw({
-    polygon: { allowIntersection: false },
-    polyline: false,
-    circle: false,
-    rectangle: false,
-    marker: false
+    draw: {
+      polygon: {
+        allowIntersection: false,
+        shapeOptions: { color:color }
+      },
+      polyline: true,
+      rectangle: true,
+      circle: true,
+      marker: true
+    },
+    edit: {
+      featureGroup: drawnItems
+    }
   });
   map.addControl(drawControl);
 
-  map.on('draw:poly-created', function (e) {
-    // hack: get coordinates from event object
-    var polygon = e.poly._latlngs;
-
-    // create proper GeoJSON object and add it
-    var coordinates = [];
-    for (var i = 0, il = polygon.length; i < il; i++) {
-      coordinates.push([polygon[i].lng, polygon[i].lat]);
-    }
-    coordinates.push([polygon[0].lng, polygon[0].lat]);
-
-    var feature = {
-      type: 'Feature',
-      geometry: {
-        type: 'Polygon',
-        coordinates: [coordinates]
-      },
-      properties: { color:color, height:height }
+  map.on('draw:created', function (e) {
+    var feature = e.layer.toGeoJSON();
+    feature.properties = {
+      color: color,
+      height: height
     };
 
-    geoJson.features.push(feature);
+    var geoJson = {
+      type: 'FeatureCollection',
+      features: [feature]
+    };
+
     osmb.setData(geoJson);
   });
 
-  map.removeControl(drawControl);
+//  map.removeControl(drawControl);
 
   hljs.highlightBlock(document.getElementsByTagName('CODE')[0]);
 });
